6e54cb517dde3be9b7bec7c9f58d8138a75ed600
Jospeh <134275356+ItsRon23@users.noreply.github.com>
2025-04-18 17:20:12 +0700

    Refactor Poor Naming in TaxFunction and TaxPayerData
    Mengubah nama variable yang sulit dipahami dan panjang di TaxPayerData dan TaxFunction

--- src/lib/TaxPayerData.java
+++ src/lib/TaxPayerData.java
@@ -3,18 +3,22 @@ package lib;
 public class TaxPayerData {
 	public int monthlySalary;
 	public int otherMonthlyIncome;
-	public int numberOfMonthWorking;
-	public int annualDeductible;
+	
+    //refactoring 5 poor naming
+    public int monthsWorkedInYear;
+    public int deductibleAmount;
+
 	public boolean isMarried;
 	public int numberOfChildren;
 
-	public TaxPayerData(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking,
-	                    int annualDeductible, boolean isMarried, int numberOfChildren) {
-		this.monthlySalary = monthlySalary;
-		this.otherMonthlyIncome = otherMonthlyIncome;
-		this.numberOfMonthWorking = numberOfMonthWorking;
-		this.annualDeductible = annualDeductible;
-		this.isMarried = isMarried;
-		this.numberOfChildren = numberOfChildren;
-	}
+    // refactoring 5 poor naming
+	public TaxPayerData(int monthlySalary, int otherMonthlyIncome, int monthsWorkedInYear,
+                    int deductibleAmount, boolean isMarried, int numberOfChildren) {
+        this.monthlySalary = monthlySalary;
+        this.otherMonthlyIncome = otherMonthlyIncome;
+        this.monthsWorkedInYear = monthsWorkedInYear;
+        this.deductibleAmount = deductibleAmount;
+        this.isMarried = isMarried;
+        this.numberOfChildren = numberOfChildren;
+    }
 }
